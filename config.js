77b6b65c9f46c9b291e7e4b6e883b6f27897d923
Your Name <you@example.com>
2021-03-01 14:07:18 -0300

    config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
     style: 'mapbox://styles/mapbox/satellite-v9',
-    accessToken: 'pk.eyJ1IjoiZ2lzbWFzdGVybTIiLCJhIjoiY2plZHhubTQxMTNoYzMza3Rqa3kxYTdrOCJ9.53B1E6mKD_EQOVb2Y0-SsA',
+    accessToken: 'pk.eyJ1IjoieW9oYW5uYWdmIiwiYSI6ImNra2N0ajZ5bTBxYXEyb28wYnFicXdvZGsifQ.tC5xp86Hm9oiu8VFfQitAA',
     showMarkers: false,
     markerColor: '#3FB1CE',
     theme: 'dark',
@@ -39,6 +39,24 @@ var config = {
                 //     opacity: 0
                 // }
             ]
+        },{
+            id: 'volcan2',
+            alignment: 'right',
+            hidden: false,
+            title: 'Monte Vesubio, Italia',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Cr%C3%A1ter_del_Vesubio.jpg',
+            description: 'Es un volcán activo del tipo vesubiano situado frente a la bahía de Nápoles y a unos nueve kilómetros de distancia de la ciudad de Nápoles. Se encuentra en la ciudad metropolitana de Nápoles, perteneciente a la región italiana de la Campania. Tiene una altura máxima de 1281 m s. n. m. y se alza al sur de la cadena principal de los Apeninos.',
+            location: {
+                center: [14.42555555, 40.82138888],
+                zoom: 14.5,
+                pitch: 60,
+                bearing: -43.2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
         }
     ]
 };
